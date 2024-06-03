5f36e8d4338bf08da6de679f93631d3af5b25420
jayakrishnagaddam <115868714+jayakrishnagaddam@users.noreply.github.com>
2024-06-03 18:53:54 +0530

    Data added back

--- updateDB.py
+++ updateDB.py
@@ -1,4 +1,3 @@
-import random
 from pymongo import MongoClient
 
 # Connection string
@@ -10,3 +9,15 @@ client = MongoClient(uri)
 # Select the database and collection
 db = client.Movie_Library
 collection = db.Movies
+
+# Define the filter to match documents where the Cast field contains "Christian Bale"
+filter_criteria = {"movie_name": {"$regex": ".*Life is Beautifull*"}}
+
+# Define the update operation to set the Link field
+update_operation = {"$set": {"link": "https://facts.net/wp-content/uploads/2023/06/48-facts-about-the-movie-life-is-beautiful-1687588625.jpg"}}
+
+# Update the documents matching the filter criteria
+result = collection.update_many(filter_criteria, update_operation)
+
+# Output the number of documents updated
+print("Number of documents updated:", result.modified_count)
