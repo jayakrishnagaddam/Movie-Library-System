dc4030da65a5a876e6d7c893a9a0c08abd971b9c
jayakrishnagaddam <115868714+jayakrishnagaddam@users.noreply.github.com>
2024-06-03 18:41:32 +0530

    Response and DB update

--- updateDB.py
+++ updateDB.py
@@ -11,10 +11,10 @@ db = client.Movie_Library
 collection = db.Movies
 
 # Define the filter to match documents where the Cast field contains "Christian Bale"
-filter_criteria = {"cast": {"$regex": ".*Hrithik Roshan*"}}
+filter_criteria = {"movie_name": {"$regex": ".*Life is Beautifull*"}}
 
 # Define the update operation to set the Link field
-update_operation = {"$set": {"link": "https://img.indiaforums.com/media/640x0/49/6405-hrithik-roshan.jpg"}}
+update_operation = {"$set": {"link": "https://facts.net/wp-content/uploads/2023/06/48-facts-about-the-movie-life-is-beautiful-1687588625.jpg"}}
 
 # Update the documents matching the filter criteria
 result = collection.update_many(filter_criteria, update_operation)
